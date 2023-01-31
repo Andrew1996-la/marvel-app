b202e6b0c0c51657d703115ba0428c527983d260
Andrew1996-la <asoborev@mail.ru>
2023-01-31 08:07:54 +0300

    Добавил вывод случайного героя в компоненте RandomChar

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,42 +1,80 @@
-import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
-import mjolnir from '../../resources/img/mjolnir.png';
+import "./randomChar.scss";
+import mjolnir from "../../resources/img/mjolnir.png";
+import MarvelServices from "../../services/MarvelServices";
+import { Component } from "react";
 
-const RandomChar = () => {
+class RandomChar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleUpdateHero();
+    this.state = {
+      char: {
+        name: null,
+        description: null,
+        thumbnail: null,
+        homepage: null,
+        wiki: null,
+      },
+    };
+  }
+  //227
+  marvelServices = new MarvelServices();
+
+  handleUpdateHero = () => {
+    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    this.marvelServices.getCharacter(id).then((res) => {
+      if (res.description === "") {
+        res.description = "there is no description of this hero";
+      }
+
+      if (res.description.length > 200) {
+        res.description = res.description.slice(0, 220) + "...";
+      }
+
+      this.setState(res);
+    });
+  };
+
+  render() {
+    const { name, description, thumbnail, homepage, wiki } = this.state;
     return (
-        <div className="randomchar">
-            <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
-                <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
-                    <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
-                    </p>
-                    <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href="#" className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
-                </div>
+      <div className="randomchar">
+        <div className="randomchar__block">
+          <img
+            src={thumbnail}
+            alt="Random character"
+            className="randomchar__img"
+          />
+          <div className="randomchar__info">
+            <p className="randomchar__name">{name}</p>
+            <p className="randomchar__descr">{description}</p>
+            <div className="randomchar__btns">
+              <a href={homepage} className="button button__main">
+                <div className="inner">homepage</div>
+              </a>
+              <a href={wiki} className="button button__secondary">
+                <div className="inner">Wiki</div>
+              </a>
             </div>
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main">
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
+          </div>
         </div>
-    )
+        <div className="randomchar__static">
+          <p className="randomchar__title">
+            Random character for today!
+            <br />
+            Do you want to get to know him better?
+          </p>
+          <p className="randomchar__title">Or choose another one</p>
+          <button className="button button__main">
+            <div className="inner" onClick={this.handleUpdateHero}>
+              try it
+            </div>
+          </button>
+          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RandomChar;
--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -1,24 +1,42 @@
 class MarvelServices {
-    _apiBaseUrl = 'https://gateway.marvel.com:443/v1/public/'
-    _apiKey = `apikey=2786610d6ef585695878ce40c5106231`
+  _apiBaseUrl = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = `apikey=2786610d6ef585695878ce40c5106231`;
 
-    getResource = async (url) => {
-        const res = await fetch(url)
+  getResource = async (url) => {
+    const res = await fetch(url);
 
-        if(!res.ok) {
-            throw new Error(`${url} don't correct`)
-        }
-
-        return await res.json()
+    if (!res.ok) {
+      throw new Error(`${url} don't correct`);
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBaseUrl}characters?${this._apiKey}`)
-    }
+    return await res.json();
+  };
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBaseUrl}characters/${id}?${this._apiKey}`)
-    }
+  getAllCharacters = async () => {
+    const res = await this.getResource(
+      `${this._apiBaseUrl}characters?limit=9&offset=210&${this._apiKey}`
+    );
+
+    return res.data.results.map((item) => this._transformCharacter(item));
+  };
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBaseUrl}characters/${id}?${this._apiKey}`
+    );
+
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (character) => {
+    return {
+      name: character.name,
+      description: character.description,
+      thumbnail: `${character.thumbnail.path}.${character.thumbnail.extension}`,
+      homepage: character.urls[0].url,
+      wiki: character.urls[1].url,
+    };
+  };
 }
 
-export default MarvelServices
+export default MarvelServices;
