0b642f279cb33f6dcf30a28d89ef84c75e494637
Andrew1996-la <asoborev@mail.ru>
2023-02-19 00:19:26 +0300

    Добавил ErrorBoundary на случай возникновия ошибки

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from "../../resources/img/vision.png";
 import { Component } from "react";
@@ -21,10 +22,16 @@ class App extends Component {
       <div className="app">
         <AppHeader />
         <main>
-          <RandomChar />
+          <ErrorBoundary>
+            <RandomChar />
+          </ErrorBoundary>
           <div className="char__content">
-            <CharList setCharId={this.setCharId} />
-            <CharInfo charId={this.state.charId} />
+            <ErrorBoundary>
+              <CharList setCharId={this.setCharId} />
+            </ErrorBoundary>
+            <ErrorBoundary>
+              <CharInfo charId={this.state.charId} />
+            </ErrorBoundary>
           </div>
           <img className="bg-decoration" src={decoration} alt="vision" />
         </main>
