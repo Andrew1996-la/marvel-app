45d1b979ccf91da474f9f9f7b553418a95a13c0d
Andrew1996-la <asoborev@mail.ru>
2023-02-18 23:50:19 +0300

    добавил проверку на наличик комиксов CharInfo

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -83,10 +83,18 @@ const View = ({ char }) => {
     description = "There is no description of this hero";
   }
 
+  const imgIsNotAvaliable =
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg";
+
+  let classImg = { objectFit: "cover" };
+  if (thumbnail === imgIsNotAvaliable) {
+    classImg = { objectFit: "contain" };
+  }
+
   return (
     <>
       <div className="char__basics">
-        <img src={thumbnail} alt={name} />
+        <img src={thumbnail} alt={name} style={classImg} />
         <div>
           <div className="char__info-name">{name}</div>
           <div className="char__btns">
@@ -102,10 +110,8 @@ const View = ({ char }) => {
       <div className="char__descr">{description}</div>
       <div className="char__comics">Comics:</div>
       <ul className="char__comics-list">
+        {comics.length === 0 && "No comics"}
         {comics.map((item, i) => {
-          if (i > 10) {
-            return;
-          }
           return (
             <li key={i} className="char__comics-item">
               {item.name}
